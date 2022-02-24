1aca4c1af126bba2c4e8f3f7cd7f6f7d405db50b
anisamufarida <100363131+anisamufarida@users.noreply.github.com>
2022-02-24 21:35:01 +0700

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'f8598529a56c89ea3a57d2245c9fadb5',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformat.com/f8598529a56c89ea3a57d2245c9fadb5/invoke.js"></scr' + 'ipt>');
