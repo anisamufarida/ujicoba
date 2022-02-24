74574196ee26947de6ee722017f787b5aff562a0
anisamufarida <100363131+anisamufarida@users.noreply.github.com>
2022-02-24 21:36:21 +0700

    Create sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '542edd1106a860720ca7f2b4147b66d6',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformat.com/542edd1106a860720ca7f2b4147b66d6/invoke.js"></scr' + 'ipt>');
